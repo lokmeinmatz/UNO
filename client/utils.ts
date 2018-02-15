6053880c833840333bc7d6d3d17b72d6d269827a
lokmeinmatz <matze.kind@web.de>
2018-02-15 19:12:58 +0100

    Created own repo

---
+++ client/utils.ts
@@ -0,0 +1,6 @@
+export interface GameUpdate {
+    players: {name: string, id: string, cards: number}[]
+    activePlayerID : string //player id
+    direction : boolean //clockwise
+    handcards: string[]
+}
